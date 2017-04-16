869b177b777c5915120e38df83b5321469749644
Young Jun Park <june0313@gmail.com>
2017-04-16 21:51:59 +0900

    [7.6] 동적 SQL 사용 #2 (누락된 파일 커밋)

--- src/main/java/spms/dao/MySqlProjectDao.java
+++ src/main/java/spms/dao/MySqlProjectDao.java
@@ -1,5 +1,6 @@
 package spms.dao;
 
+import com.google.common.collect.Maps;
 import lombok.Setter;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -7,6 +8,8 @@ import spms.annotation.Component;
 import spms.vo.Project;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Component("projectDao")
 @Setter
@@ -15,9 +18,9 @@ public class MySqlProjectDao implements ProjectDao {
 	private SqlSessionFactory sqlSessionFactory;
 
 	@Override
-	public List<Project> selectList() throws Exception {
+	public List<Project> selectList(Map<String, Object> paramMap) throws Exception {
 		try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-			return sqlSession.selectList("spms.dao.ProjectDao.selectList");
+			return sqlSession.selectList("spms.dao.ProjectDao.selectList", paramMap);
 		}
 	}
 
@@ -40,9 +43,24 @@ public class MySqlProjectDao implements ProjectDao {
 	@Override
 	public int update(Project project) throws Exception {
 		try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-			int count = sqlSession.update("spms.dao.ProjectDao.update", project);
-			sqlSession.commit();
-			return count;
+			Project original = sqlSession.selectOne("spms.dao.ProjectDao.selectOne", project.getNo());
+			Map<String, Object> paramMap = Maps.newHashMap();
+
+			Optional.ofNullable(project.getTitle()).filter(title -> !title.equals(original.getTitle())).ifPresent(title -> paramMap.put("title", title));
+			Optional.ofNullable(project.getContent()).filter(content -> !content.equals(original.getContent())).ifPresent(content -> paramMap.put("content", content));
+			Optional.ofNullable(project.getStartDate()).filter(date -> date.compareTo(original.getStartDate()) != 0).ifPresent(date -> paramMap.put("startDate", date));
+			Optional.ofNullable(project.getEndDate()).filter(date -> date.compareTo(original.getEndDate()) != 0).ifPresent(date -> paramMap.put("endDate", date));
+			Optional.of(project.getState()).filter(state -> state != (original.getState())).ifPresent(state -> paramMap.put("state", state));
+			Optional.ofNullable(project.getTags()).filter(tags -> !tags.equals(original.getTags())).ifPresent(tags -> paramMap.put("tags", tags));
+
+			if (paramMap.size() > 0) {
+				paramMap.put("no", project.getNo());
+				int count = sqlSession.update("spms.dao.ProjectDao.update", paramMap);
+				sqlSession.commit();
+				return count;
+			}
+
+			return 0;
 		}
 	}
 
--- src/main/java/spms/dao/ProjectDao.java
+++ src/main/java/spms/dao/ProjectDao.java
@@ -3,9 +3,10 @@ package spms.dao;
 import spms.vo.Project;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ProjectDao {
-	List<Project> selectList() throws Exception;
+	List<Project> selectList(Map<String, Object> paramMap) throws Exception;
 	int insert(Project project) throws Exception;
 	Project selectOne(int no) throws Exception;
 	int update(Project project) throws Exception;
