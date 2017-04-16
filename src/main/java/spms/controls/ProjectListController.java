21c150f05b6a360c946adb5436fa9ac8344d1f90
Young Jun Park <june0313@gmail.com>
2017-04-16 21:50:42 +0900

    [7.6] 동적 SQL 사용

--- src/main/java/spms/controls/ProjectListController.java
+++ src/main/java/spms/controls/ProjectListController.java
@@ -1,8 +1,10 @@
 package spms.controls;
 
+import com.google.common.collect.Maps;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import spms.annotation.Component;
+import spms.bind.DataBinding;
 import spms.dao.ProjectDao;
 import spms.vo.Project;
 
@@ -12,15 +14,24 @@ import java.util.Map;
 @Setter
 @Accessors(chain = true )
 @Component("/project/list.do")
-public class ProjectListController implements Controller {
+public class ProjectListController implements Controller, DataBinding {
 
 	private ProjectDao projectDao;
 
 	@Override
 	public String execute(Map<String, Object> model) throws Exception {
-		List<Project> projects = projectDao.selectList();
+		Map<String, Object> paramMap = Maps.newHashMap();
+		paramMap.put("orderCond", model.get("orderCond"));
+
+		List<Project> projects = projectDao.selectList(paramMap);
 		model.put("projects", projects);
 		return "/project/ProjectList.jsp";
 	}
 
+	@Override
+	public Object[] getDataBinders() {
+		return new Object[] {
+			"orderCond", String.class
+		};
+	}
 }
